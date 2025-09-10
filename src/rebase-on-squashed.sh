cfc81321f9a22cb31136f1ecf1cad5c8363cbd31
marcin <marcin.konowalczyk@canonical.com>
2025-09-10 10:53:40 +0100

    feat: --quiet mode

--- src/rebase-on-squashed.sh
+++ src/rebase-on-squashed.sh
@@ -19,14 +19,17 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 function _info() {
+    [[ $QUIET -eq 1 ]] && return
     echo -e "${BLUE}INFO:${NC} $1"
 }
 
 function _error() {
+    [[ $QUIET -eq 1 ]] && return
     echo -e "${RED}ERROR:${NC} $1" >&2
 }
 
 function _warn() {
+    [[ $QUIET -eq 1 ]] && return
     echo -e "${YELLOW}WARNING:${NC} $1" >&2
 }
 
@@ -41,26 +44,32 @@ function _not_implemented() {
 
 ################################################################################
 
+_HELP="
+usage: rebase-on-squashed.sh [-h | --help] [v | --version]
+       [--hard] [--trunk <trunk-branch>] [-C <path>]
+       <base-branch...>
+
+Rebases the current branch on top of a squashed version of the base branch.
+
+Options:
+  --hard                Considers the files in the base branch as authoritative.
+                        Any changes to those files in the current branch will be
+                        discarded.
+  --trunk <branch>      Specify the trunk branch (default: main).
+  -C <path>             Change to directory <path> before doing anything.
+                        (default: current directory)
+  -b, --branch <branch> Specify the branch to rebase. If not provided, the
+                        current branch will be used.
+  -q, --quiet           Suppress informational messages.
+  -h, --help            Show this help message and exit.
+  -v, --version         Show version information and exit.
+"
 function usage () {
-    echo "usage: rebase-on-squashed.sh [-h | --help] [v | --version]"
-    echo "       [--hard] [--trunk <trunk-branch>] [-C <path>]"
-    echo "       <base-branch...>"
-    echo ""
-    echo "Rebases the current branch on top of a squashed version of the base branch."
-    echo ""
-    echo "Options:"
-    echo "  --hard            Considers the files in the base branch as authoritative."
-    echo "                    Any changes to those files in the current branch will be"
-    echo "                    discarded."
-    echo "  --trunk <branch>  Specify the trunk branch (default: main)."
-    echo "  -C <path>         Change to directory <path> before doing anything."
-    echo "                    (default: current directory)"
-    echo "  -b, --branch <branch> Specify the branch to rebase. If not provided, the"
-    echo "                    current branch will be used."
-    echo "  -h, --help        Show this help message and exit."
-    echo "  -v, --version     Show version information and exit."
+    # remove the first and last empty lines
+    echo "$_HELP" | sed '/./,$!d' | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba'
 }
 
+QUIET=0
 HARD_MODE=0
 TRUNK_BRANCH="main"
 BASE_BRANCHES=()
@@ -68,8 +77,6 @@ BASE_BRANCH=""
 CURRENT_BRANCH=""
 TARGET_BRANCH=""
 REPO_PATH="$PWD"
-
-_TEMP_BRANCH=""
 
 function setup() {
 
@@ -90,6 +97,7 @@ function setup() {
             -v|--version)
                 echo "rebase-on-squashed.sh version $__VERSION__"
                 exit 0 ;;
+            -q|--quiet) QUIET=1; shift ;;
             -C) 
                 _requires_arg "-C" "$2"
                 REPO_PATH="$2"; shift 2 ;;
@@ -110,18 +118,13 @@ function setup() {
         exit 1
     fi
 
-    if ! command -v git &> /dev/null; then
-        echo "Error: git is not installed."
-        exit 1
-    fi
+    # check if git is installed
+    command -v git &> /dev/null || _fatal "Error: git is not installed."
 
     BASE_BRANCHES=("$@")
 
     # for now we support only one base branch
-    if [[ "${#BASE_BRANCHES[@]}" -ne 1 ]]; then
-        echo "Error: Only one base branch is supported for now."
-        exit 1
-    fi
+    [[ "${#BASE_BRANCHES[@]}" -eq 1 ]] || _fatal "Error: Only one base branch is supported for now."
 
     BASE_BRANCH="${BASE_BRANCHES[0]}"
 
@@ -143,8 +146,8 @@ function main () {
 
     if [[ -n "$TARGET_BRANCH" ]]; then
         _info "Checking out target branch $TARGET_BRANCH"
-        git checkout "$TARGET_BRANCH"
-        trap 'git checkout "$CURRENT_BRANCH"' EXIT
+        git checkout --quiet "$TARGET_BRANCH"
+        trap 'git checkout --quiet "$CURRENT_BRANCH"' EXIT
     else
         TARGET_BRANCH="$CURRENT_BRANCH"
     fi
@@ -178,33 +181,42 @@ function main () {
     fi
     _info "Found common ancestor between $TRUNK_BRANCH and $TARGET_BRANCH: $current_merge_base"
 
+    # check in the history of the current branch if we already have a squash commit for the base branch
+    local message_head="feat(ros)!: Squashed branch '$BASE_BRANCH'"
+    local message_tail="$(git rev-parse --short "$BASE_BRANCH")"
+
+    local existing_squash_commit
+    existing_squash_commit=$(git log --grep="^$message_head" --pretty=format:"%H" -1 || true)
+    if [[ -n "$existing_squash_commit" ]]; then
+        local existing_squash_commit_message
+        existing_squash_commit_message=$(git log -1 --pretty=format:"%s" "$existing_squash_commit" || true)
+        local existing_squash_commit_tail
+        existing_squash_commit_tail=$(echo "$existing_squash_commit_message" | sed -E "s/.* at (.+)$/\1/" || true)
+        if [[ "$existing_squash_commit_tail" == "$message_tail" ]]; then
+            _info "Branch '$TARGET_BRANCH' already has a squash commit for '$BASE_BRANCH' ($existing_squash_commit). Nothing to do."
+            exit 0
+        fi
+        _not_implemented "Rebase on existing squash commit $existing_squash_commit is not implemented yet."
+    fi
+
     # create a new squashed version of the base branch
     local temp_branch="temp/rebase-on-squashed/${TARGET_BRANCH//\//-}-on-${BASE_BRANCH//\//-}"
     if git show-ref --verify --quiet "refs/heads/$temp_branch"; then
         _info "Deleting existing temporary branch $temp_branch"
-        git branch -D "$temp_branch"
+        git branch --quiet -D "$temp_branch"
     fi
 
     _info "Creating temporary branch $temp_branch with squashed changes from $BASE_BRANCH"
-    git checkout -b "$temp_branch" "$BASE_BRANCH"
+    git checkout --quiet -b "$temp_branch" "$BASE_BRANCH"
     # shellcheck disable=SC2064
-    trap "git checkout \"$TARGET_BRANCH\"; git branch -D \"$temp_branch\" || true" EXIT
+    trap "git checkout --quiet \"$TARGET_BRANCH\"; git branch --quiet -D \"$temp_branch\" || true" EXIT
     git reset --soft "$(git merge-base "$BASE_BRANCH" "$TARGET_BRANCH")"
 
-    local message_head="feat(ros)!: Squashed branch $BASE_BRANCH"
-    local message_tail="at $(git rev-parse --short "$BASE_BRANCH")"
-
-    # check in the history of the current branch if we already have a squash commit for the base branch
-    local existing_squash_commit
-    existing_squash_commit=$(git log --grep="^$message_head" --pretty=format:"%H" -1 || true)
-    if [[ -n "$existing_squash_commit" ]]; then
-        _not_implemented "A squash commit for $BASE_BRANCH already exists in the history of $TARGET_BRANCH: $existing_squash_commit. Reusing existing squash commits is not implemented yet."
-    fi
-
     git commit \
+        --quiet \
         --allow-empty \
         --author="$(git log -1 --pretty=format:'%an <%ae>' "$BASE_BRANCH")" \
-        -m "$message_head $message_tail"
+        -m "$message_head at $message_tail"
 
     _info "Rebasing $TARGET_BRANCH on top of $temp_branch"
     if [[ "$HARD_MODE" -eq 1 ]]; then
@@ -216,7 +228,7 @@ function main () {
         base_files=$(echo "$base_files" | tr '\n' ' ')
         if [[ -z "$base_files" ]]; then
             _warn "No files changed between $base_merge_base and $BASE_BRANCH. Nothing to discard."
-            git checkout "$TARGET_BRANCH"
+            git checkout --quiet "$TARGET_BRANCH"
             git rebase "$temp_branch"
             return
         fi
@@ -228,7 +240,7 @@ function main () {
             _info "Deleting existing backup branch $current_backup_branch"
             git branch -D "$current_backup_branch"
         fi
-        git checkout -b "$current_backup_branch" "$TARGET_BRANCH"
+        git checkout --quiet -b "$current_backup_branch" "$TARGET_BRANCH"
 
         # modify the history of the current branch (up to the merge base with the trunk branch)
         # to discard changes to the base files. If the modified commit becomes empty, discard it.
@@ -247,16 +259,17 @@ function main () {
             -- "$current_merge_base..$TARGET_BRANCH"
     fi
 
-    git checkout "$TARGET_BRANCH"
+    git checkout --quiet "$TARGET_BRANCH"
     # rebase, but only the commits up to the merge base with the trunk branch
     local cmd="git rebase \
+        --quiet \
         -X theirs \
         --onto \"$temp_branch\" \"$current_merge_base\" \"$TARGET_BRANCH\""
     _info "Running: $cmd"
     eval "$cmd"
 
     if [[ "$HARD_MODE" -eq 1 ]]; then
-        git branch -D "$current_backup_branch" || true
+        git branch --quiet -D "$current_backup_branch" || true
     fi
 }
 
